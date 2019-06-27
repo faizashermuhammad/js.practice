84008a3c10ff487b77ceff7a19790e29a24fb832
faizashermuhammad <49517190+faizashermuhammad@users.noreply.github.com>
2019-06-27 11:56:10 +0500

    Add files via upload

---
+++ script-2.js
@@ -0,0 +1,51 @@
+
+//Write a program that prints Hello! 100 times.
+// for(let i = 0 ; i < 100 ; i++){
+//  console.log("hello world");
+// }
+//write a program that prints numbers from 1 to 1000
+
+// for(let count = 1 ;  count < 1000 ; count++){
+//     console.log(count)
+// }
+
+//write a program that prints numbers from 1000 to 1
+// for(let count= 1000 ; count > 0 ; count--){
+//     console.log(count)
+
+//Write a program that prints the sum of all numbers from 1 to 100.
+// for( let sum= 0 ;  sum < 100 ; sum++){
+//     console.log(sum+1);
+// }
+//Write a program that prints the product of all numbers from 1 to 100.
+
+
+
+//Write a program that prints all EVEN numbers between 1 and 99.
+//  let = evenNumber
+// for( counter = 1; counter < 99 ; counter++){
+//     console.log(counter+1);
+// }
+// Write a program that counts how many EVEN numbers there are between 7 and 82 and then prints the coun
+
+
+// tables
+// function printTable(){
+//     let numb = parseInt(document.querySelector("input").value);
+//     for( let i =1 ; i <= 10 ; i++){
+//         console.log( numb * i);
+//     }
+    
+// }
+
+// function myFunct(){
+
+//     let minNumb= parseInt(document.querySelectoe("minInp").value);
+//     let maxNumb= parseInt(document.querySelector("maxInp").value);
+//     let stepNumb= parseInt(document.querySelector("stepInp").value);
+
+//     For (let count=  minNum; count <=maxNum; count-=stepNum){
+//         console.log(count);
+//     }
+// }
+
---
+++ script.js
@@ -0,0 +1,12 @@
+// let coins=0;
+
+// function addCoins(){
+//     coins=coins+1;
+//     document.querySelector('p').innerHTML=coins;
+// }
+
+// let isRaining= false;
+// let isSnowing= false;
+// let windSpeed= 15;
+// let canLaunch=(isRaining===false)&&(isSnowing===false)&&(windSpeed=== < 20);
+// alert(canLaunch);
